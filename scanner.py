c41bb8db22f9738f697f06cbcef4d6a51f9f2681
Pragati <12khushi11sharma@gmail.com>
2026-08-12 13:31:28 +0530

    convert gray

--- scanner.py
+++ scanner.py
@@ -1,9 +1,21 @@
 import cv2
 import numpy as np
 
+# Read document
 image = cv2.imread("images/document.jpg")
 
 cv2.imshow("Original", image)
 
 cv2.waitKey(0)
 cv2.destroyAllWindows()
+
+
+# Convert document into gray
+gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+blur = cv2.GaussianBlur(gray, (5, 5), 0)
+edges = cv2.Canny(blur, 75, 200)
+
+cv2.imshow("Edges", edges)
+cv2.waitKey(0)
+cv2.destroyAllWindows()
+
